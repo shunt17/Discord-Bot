d6bda05db30c1a27e75f3b0df6caff2a96172c37
shunt17 <32737581+shunt17@users.noreply.github.com>
2018-05-02 07:07:52 -0400

    Create errors.js

---
+++ utils/errors.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+
+module.exports.noPerms = (message, perm) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setTitle("Insufficient Permission")
+        .setColor('FF0000')
+        .addField("Permission needed", perm);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.equalPerms = (message, user, perms) => {
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor('FF0000')
+        .setTitle("Error")
+        .addField(`${user} has perms`, perms);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+
+}
+
+module.exports.botuser = (message) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("You cannot ban a bot.")
+        .setColor('FF0000');
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.cantfindUser = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("Could not find that user.")
+        .setColor('FF0000');
+
+    channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.noReason = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("Please supply a reason.")
+        .setColor('FF0000');
+
+    channel.send(embed).then(m => m.delete(5000));
+}
